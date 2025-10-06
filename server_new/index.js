8e94248e03660a6cf53ab898a38cd50c94d0e21d
Hanniel <2022371026@UTEQ.EDU.MX>
2025-10-06 10:38:39 -0600

    Change default port to 8080 for Railway compatibility

--- server_new/index.js
+++ server_new/index.js
@@ -183,5 +183,5 @@ app.post('/api/images', (req, res) => {
   }
 });
 
-const port = process.env.PORT || 9002;
+const port = process.env.PORT || 8080;
 app.listen(port, ()=> console.log('Server new listening on', port));
