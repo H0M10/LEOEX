46e0d682e7f209244adf03a0c4f1165acea23256
Hanniel <2022371026@UTEQ.EDU.MX>
2025-10-06 14:22:26 -0600

    Fix TerrainMap import and update docs build

--- client/src/components/SatelliteVisualization.jsx
+++ client/src/components/SatelliteVisualization.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import TerrainMap from './TerrainMap';
 
 const SatelliteVisualization = ({ region, sector }) => {
   const [currentTime, setCurrentTime] = useState(new Date());
